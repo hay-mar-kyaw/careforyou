bfdc274e6f94d6cb06d08ecec4dc7f6a433bf7a6
hay-mar-kyaw <mandalaytvsindiamotorcycles@gmail.com>
2025-02-25 22:17:54 +0700

    layout template

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <main class="h-full overflow-y-auto">
+        <div class="container px-6 mx-auto grid">
+          <h2
+            class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+          >
+            Dashboard
+          </h2>
+          <!-- Cards -->
+          <x-card-section/>
+
+          <!-- New Table -->
+          <x-table-section/>
+
+          <!-- Charts -->
+          <x-chart-section/>
+
+        </div>
+    </main>
+</x-layout>
